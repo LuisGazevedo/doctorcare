f24f3c839e671f2efbb73afc273e16ca67f67412
LuisGazevedo <104107572+LuisGazevedo@users.noreply.github.com>
2022-05-08 14:33:09 -0300

    Substituição de arquivos corrompidos

--- main.js
+++ main.js
@@ -1,61 +1,44 @@
-window.addEventListener('scroll', onScroll )
-onScroll()
+window.addEventListener('scroll', onScroll)
 
+onScroll()
 function onScroll() {
   showNavOnScroll()
-  showBackToTopButtonScroll()
+  showBackToTopButtonOnScroll()
 
-  activateMenuAtCuttentSection(home)
-  activateMenuAtCuttentSection(services)
-  activateMenuAtCuttentSection(about)
-  activateMenuAtCuttentSection(contact)
-  //activateMenuAtCuttentSection(services)
-  }
-
-function activateMenuAtCuttentSection(section) {
-  //Linha Alvo
-    const targetLine = scrollY + innerHeight /2
-  
-  //Verificar se a seção passou da linha
-  //Quais dados vou precisar?
-
-  //O topo da seção
-    const sectionTop = section.offsetTop
-
-  //Altura da seção   
-    const sectionHeight = section.offsetHeight
-
-  //O topod da seção chegou ou ultrapssou a linha alvo
-    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
-
-  //Informação dos dados e da lógica
-    console.log('O topo da seção chegou ou passou da linha?', sectionTopReachOrPassedTargetLine)
-
-
-  //verificar se a base está abaixo da linha alvo
-  //quais dados vou precisar?
-  //A seção termina onde?
-    const sectionEndsAt = sectionTop + sectionHeight
-
-   const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
-
-   console.log('O fundo da seção passou da linha? ', sectionEndPassedTargetLine )
-
-   //limites da seção
-    const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
-
-    const sectionId = section.getAtribute('id')
-    const menuElemnt=document.querySelector(`.menu a[href*=${sectionId}]`)
-
-    menuElemnt.classList.remove('active')
-    if(sectionBoundaries) {
-      menuElemnt.classList.add('active')
-    }
-
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
 }
 
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
 
-function showNavOnScroll(){
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // verificar se a base está abaixo da linha alvo
+
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
@@ -63,34 +46,33 @@ function showNavOnScroll(){
   }
 }
 
-function showBackToTopButtonScroll(){
+function showBackToTopButtonOnScroll() {
   if (scrollY > 550) {
     backToTopButton.classList.add('show')
   } else {
     backToTopButton.classList.remove('show')
   }
 }
-   
-  function openMenu() {
-    document.body.classList.add('menu-expanded')
-  }
-  
-  function closeMenu() {
-    document.body.classList.remove('menu-expanded')
-  }
 
-  ScrollReveal({
-    origin: 'top',
-    distance: '30px',
-    duration: 1200,
-  }).reveal(`
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
   #home, 
   #home img, 
-  #home .stats,
+  #home .stats, 
   #services,
   #services header,
-  #services .card,
-  #about,
-  #about header,
-  #about content`)
-
+  #services .card
+  #about, 
+  #about header, 
+  #about .content`)
